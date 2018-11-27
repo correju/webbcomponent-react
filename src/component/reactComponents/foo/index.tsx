fb552f47f5ffbaa4ac45a65ffd74e458f50d3488
julian correa <correa.julian@gmail.com>
2018-11-26 22:54:46 -0500

    added class component

--- src/component/reactComponents/foo/index.tsx
+++ src/component/reactComponents/foo/index.tsx
@@ -6,7 +6,7 @@ import styled from 'styled-components';
  * Todo move interfaces to its own folder
  */
 interface IProps{
-  comments: Array<IComments>
+  comments: Array<IComments>;
 }
 
 interface IState{
@@ -17,43 +17,48 @@ const Ul = styled.ul`
   list-style-type: none
   padding: 0;
   background-color: black;
-  width: calc(100% - 2rem);
+  width: 100%;
 `;
 
 const Div = styled.div`
   background-color: black;
-  width: 100%;
   padding: 1rem;
 `;
 let color: string = 'red';
 
-setTimeout(() => {
-  color = 'purple';
-  console.log('black');
-}, 5000);
-
 const Li = styled.li`
   padding: 1rem;
-  border: 1px solid ${(props) => props.color};
+  border: 0.1rem solid ${(props) => props.color};
   margin: 15px 0;
-  border-radius: 1rem;
+  border-radius: .3rem;
   color: white;
 `;
 
 const clickHandler = () => color = `#${Math.floor(Math.random()*16777215).toString(16)}`;
 
-export default (props) => (
-  <Div>
-    <button onClick={() => clickHandler()}>click me</button>
-    <Ul className="comments-list">
-    {
-      (props.comments || []).map(e => (
-        <Li color={color} key={e.id.toString()}>
-          <h5>{e.name}</h5>
-          <p>{e.body}</p>
-        </Li>
-      ))
-    }
-    </Ul>
-  </Div>
-);
+export default class Foo extends React.Component<IProps, IState>{
+  public state: IState = {color: 'red'};
+  constructor(props) {
+    super(props);
+  }
+  private clickHandler() {
+    this.setState({...this.state, color: `#${Math.floor(Math.random()*16777215).toString(16)}`});
+  }
+  public render() {
+    return (
+      <Div>
+        <button onClick={() => this.clickHandler()}>click me</button>
+        <Ul className="comments-list">
+        {
+          (this.props.comments || []).map(e => (
+            <Li color={this.state.color} key={e.id.toString()}>
+              <h5>{e.name}</h5>
+              <p>{e.body}</p>
+            </Li>
+          ))
+        }
+        </Ul>
+      </Div>
+    );
+  }
+}
